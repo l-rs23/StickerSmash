d42e484b44671fff8e2e187aabceefa536250443
Lucia <lucia.ruiz@marsibionics.com>
2023-05-24 15:33:03 +0200

    Commented code and separated components in files.

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 
+import ImageViewer from './components/ImageViewer';
+
+// We use "require" to reference the image as the asset is a static resource. This means
+// that it comes from a static place, and not the network as if we were extracting the
+// picture from a web place or smt similar. This function can be used to open images,
+// videos, audios or documents from our project.
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -16,6 +22,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+// "Flex" is used to tell stylesheet how much of the space this element can be spread out.
+// In Android this would be equivalent to using "weight".
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
@@ -23,10 +31,5 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
   },
 });
